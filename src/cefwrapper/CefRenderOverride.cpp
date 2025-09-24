038d8da890fd273b0007b41f3e5785b279fc2281
ManuzXo <emanuelerispoli2712@gmail.com>
2025-09-24 19:47:52 +0200

    clear code

--- src/cefwrapper/CefRenderOverride.cpp
+++ src/cefwrapper/CefRenderOverride.cpp
@@ -1,1 +1,9 @@
 #include "CefRenderOverride.h"
+
+void CefRenderOverride::OnContextCreated(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, CefRefPtr<CefV8Context> context){
+    CefRefPtr<CefV8Value> global = context->GetGlobal();
+    CefRefPtr<CefV8Override> handler = new CefV8Override();
+
+    handler->RegisterFunction("TestV8", new TestV8());
+    global->SetValue("TestV8", CefV8Value::CreateFunction("TestV8", handler), V8_PROPERTY_ATTRIBUTE_NONE);
+}
--- src/cefwrapper/CefRenderOverride.h
+++ src/cefwrapper/CefRenderOverride.h
@@ -6,18 +6,7 @@
 class CefRenderOverride : public CefRenderProcessHandler {
 public:
     CefRenderOverride() {}
-
-    void OnContextCreated(CefRefPtr<CefBrowser> browser,
-        CefRefPtr<CefFrame> frame,
-        CefRefPtr<CefV8Context> context) override
-    {
-        CefRefPtr<CefV8Value> global = context->GetGlobal();
-        CefRefPtr<CefV8Override> handler = new CefV8Override();
-
-        handler->RegisterFunction("TestV8", new TestV8());
-        global->SetValue("TestV8", CefV8Value::CreateFunction("TestV8", handler), V8_PROPERTY_ATTRIBUTE_NONE);
-    }
-
+    void OnContextCreated(CefRefPtr<CefBrowser> browser, CefRefPtr<CefFrame> frame, CefRefPtr<CefV8Context> context) override;
 private:
     IMPLEMENT_REFCOUNTING(CefRenderOverride);
 };
